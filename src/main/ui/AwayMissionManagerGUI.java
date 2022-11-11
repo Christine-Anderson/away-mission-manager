031561c9ffd80d7a103734630f3d27364950c537
Christine-Anderson <114715340+Christine-Anderson@users.noreply.github.com>
2022-11-10 18:42:22 -0800

    moved window to desktop

--- src/main/ui/AwayMissionManagerGUI.java
+++ src/main/ui/AwayMissionManagerGUI.java
@@ -1,48 +1,85 @@
 package ui;
 
 import model.Starship;
+import persistance.JsonReader;
+import persistance.JsonWriter;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.WindowEvent;
+import java.io.FileNotFoundException;
 
 /**
- * Away mission manager GUI
+ * GUI for initial window asking the user if they would like to load their data
  */
 public class AwayMissionManagerGUI extends JFrame implements ActionListener {
-    private static final int WIDTH = 400;
-    private static final int HEIGHT = 300;
-    private static final int VGAP = 15;
-    private static final int HGAP = 15;
+    private static final int DESKTOP_WINDOW_WIDTH = 800;
+    private static final int DESKTOP_WINDOW_HEIGHT = 600;
+    private static final int LOAD_WINDOW_WIDTH = 400;
+    private static final int LOAD_WINDOW_HEIGHT = 250;
+    private static final int LOAD_WINDOW_VGAP = 10;
+    private static final int LOAD_WINDOW_HGAP = 10;
+
+    private static final String JSON_STARSHIP = "./data/starship.json";
+    private Starship starship;
+    private JsonWriter jsonWriter;
+    private JsonReader jsonReader;
+
+    private JDesktopPane desktop;
+    private JInternalFrame loadWindow;
     private JPanel jpanel;
     private JButton button;
     private JLabel label;
     private ImageIcon image;
 
     //TODO dont forget method comments!
-    public AwayMissionManagerGUI() {
-        //Create and set up the window.
-        super("Away Mission Manager Application");
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    public AwayMissionManagerGUI() throws FileNotFoundException {
+        starship = new Starship("Captain", "Name");
+        jsonWriter = new JsonWriter(JSON_STARSHIP);
+        jsonReader = new JsonReader(JSON_STARSHIP);
+        desktop = new JDesktopPane();
 
-        //Set up the content pane.
-        Container pane = this.getContentPane();
-        pane.setLayout(new BoxLayout(pane, BoxLayout.Y_AXIS));
-        pane.add(Box.createRigidArea(new Dimension(0, VGAP)));
-        addComponentsToPane(pane);
-        pane.add(Box.createRigidArea(new Dimension(0, VGAP)));
+        setContentPane(desktop);
+        setTitle("Away Mission Manager Application");
+        setSize(DESKTOP_WINDOW_WIDTH, DESKTOP_WINDOW_HEIGHT);
 
-        //Display the window.
-        this.setPreferredSize(new Dimension(WIDTH,HEIGHT));
-        this.pack();
-        this.setLocationRelativeTo(null);
-        this.setResizable(false);
-        this.setVisible(true);
+        createLoadWindow();
+        desktop.add(loadWindow);
+
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setVisible(true);
+
+
+//        //Create and set up the window.
+//        super("Away Mission Manager Application");
+//        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+//        //Display the window.
+//        this.setPreferredSize(new Dimension(WIDTH,HEIGHT));
+//        this.pack();
+//        this.setLocationRelativeTo(null);
+//        this.setResizable(false);
+//        this.setVisible(true);
     }
 
-    private void addComponentsToPane(Container pane) {
+    public void createLoadWindow() {
+        loadWindow = new JInternalFrame("Loading...", false, false, false, false);
+
+        //Set up the content pane.
+        Container pane = loadWindow.getContentPane();
+        pane.setLayout(new BoxLayout(pane, BoxLayout.Y_AXIS));
+        pane.add(Box.createRigidArea(new Dimension(0, LOAD_WINDOW_VGAP)));
+        addComponentsToPane(pane);
+        pane.add(Box.createRigidArea(new Dimension(0, LOAD_WINDOW_VGAP)));
+
+        loadWindow.pack();
+        //loadWindow.setLocationRelativeTo(null);
+        loadWindow.setVisible(true);
+    }
+
+        private void addComponentsToPane(Container pane) {
         JPanel jpanel1 = addPanel(80, pane);
         //jpanel1.setBackground(Color.yellow);
         jpanel1.setLayout(new BoxLayout(jpanel1, BoxLayout.Y_AXIS));
@@ -50,20 +87,20 @@ public class AwayMissionManagerGUI extends JFrame implements ActionListener {
         addImage(jpanel1);
         addLabel("Would you like to load previous starship data?", jpanel1);
 
-        pane.add(Box.createRigidArea(new Dimension(0, VGAP)));
+        pane.add(Box.createRigidArea(new Dimension(0, LOAD_WINDOW_VGAP)));
 
         JPanel jpanel2 = addPanel(20, pane);
         //jpanel2.setBackground(Color.green);
         jpanel2.setLayout(new BoxLayout(jpanel2, BoxLayout.X_AXIS));
         addButton("yes", "loadFile", jpanel2);
-        jpanel2.add(Box.createRigidArea(new Dimension(HGAP, 0)));
+        jpanel2.add(Box.createRigidArea(new Dimension(LOAD_WINDOW_HGAP, 0)));
         addButton("no", "doNotLoadFile", jpanel2);
     }
 
     private JPanel addPanel(int percentHeight, Container pane) {
         jpanel = new JPanel();
         pane.add(jpanel);
-        jpanel.setPreferredSize(new Dimension(WIDTH,HEIGHT * percentHeight / 100));
+        jpanel.setPreferredSize(new Dimension(LOAD_WINDOW_WIDTH, LOAD_WINDOW_HEIGHT * percentHeight / 100));
         jpanel.setVisible(true);
         return jpanel;
     }
@@ -93,15 +130,18 @@ public class AwayMissionManagerGUI extends JFrame implements ActionListener {
         container.add(label);
     }
 
+    public void addCentered(Component internalFrame) {
+        desktop.add(internalFrame);
+        internalFrame.setLocation((desktop.getWidth()-internalFrame.getWidth())/2, (desktop.getHeight()-internalFrame.getHeight())/2);
+        internalFrame.setVisible(true);
+    }
+
     //This is the method that is called when the JButton btn is clicked TODO all comments
     public void actionPerformed(ActionEvent e) {
         if (e.getActionCommand().equals("loadFile")) {
-            app.loadStarship();
-            this.getContentPane().removeAll();
         }
 
         if (e.getActionCommand().equals("doNotLoadFile")) {
-            this.getContentPane().removeAll();
         }
     }
 }
--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -8,7 +8,7 @@ import java.io.FileNotFoundException;
 public class Main {
     public static void main(String[] args) {
         try {
-            new AwayMissionManagerApp();
+            new AwayMissionManagerGUI();
         } catch (FileNotFoundException e) {
             System.out.println("Unable to run application: file not found");
         }
