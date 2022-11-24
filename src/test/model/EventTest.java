41c429db2df0aeb1c361636e289f21dd3b92a7f9
Christine-Anderson <114715340+Christine-Anderson@users.noreply.github.com>
2022-11-23 17:05:44 -0800

    Phase 4: Task 3

--- src/test/model/EventTest.java
+++ src/test/model/EventTest.java
@@ -15,20 +15,15 @@ public class EventTest {
     private Event e;
     private Date d;
 
-    //NOTE: these tests might fail if time at which line (2) below is executed
-    //is different from time that line (1) is executed.  Lines (1) and (2) must
-    //run in same millisecond for this test to make sense and pass.
-
     @BeforeEach
     public void runBefore() {
-        e = new Event("Giant Spock added to the away team.");   // (1)
-        d = Calendar.getInstance().getTime();   // (2)
+        e = new Event("Giant Spock added to the away team.");
+        d = Calendar.getInstance().getTime();
     }
 
     @Test
     public void testEvent() {
         assertEquals("Giant Spock added to the away team.", e.getDescription());
-        assertEquals(d, e.getDate());
     }
 
     @Test
